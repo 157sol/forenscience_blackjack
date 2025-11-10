2f0551eaf5664442c178ed82fb478e45246570a8
157sol <kty100716@gmail.com>
2025-11-10 18:45:17 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+// super tiny offline cache + safe updates
+const CACHE = 'bj-trainer-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-180.png',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/maskable-192.png',
+  './icons/maskable-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    Promise.all([
+      caches.keys().then((keys) =>
+        Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+      ),
+      self.clients.claim()
+    ])
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then((r) => r || fetch(e.request))
+  );
+});
